44922d3ae45d35f2d369daa956c63c1e797ae5b3
isandeepmakwana <isandeepmakwana1@gmail.com>
2022-12-27 19:12:18 +0530

    Assignment completed .

--- Assignment-2-ml/MeanSquaredError.py
+++ Assignment-2-ml/MeanSquaredError.py
@@ -1,8 +1,9 @@
-#RMSE
+# MSE
 import numpy
-a=[2, 3, 5, 5, 9]
-p=[3, 3, 8, 7, 6]
-diff_list = numpy.subtract(a,p)
-squred_diff=numpy.square(diff_list)
-mse = numpy.sum(squred_diff)/len(a)
-print(numpy.sqrt(mse))
+
+a = [2, 3, 5, 5, 9]
+p = [3, 3, 8, 7, 6]
+diff_list = numpy.subtract(a, p)
+squred_diff = numpy.square(diff_list)
+mse = numpy.sum(squred_diff) / len(a)
+print(mse)
